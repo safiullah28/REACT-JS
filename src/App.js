a09af7d07c59b44a43a404b808b7e559928e9c71
safiullah28 <safiullah424@gmail.com>
2024-03-13 00:07:07 +0500

    use State with Object and Arrays

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from "react";
 function App() {
+  //Use State with Objects
+  // const [values,setValues]=useState({fname:"Safi",lname:"Ullah"});
+  // function update()
+  // {
+  //   setValues({...values,fname:"Fatim"})
+  // }
+  // ... create copy of object first and then overwrite the values of that object key
+
+  //Use State with Arrys
+  const [items,setItems]=useState([]);
+  function additems()
+  {
+    setItems([
+      ...items,{
+        id:items.length,
+        object:"Pen"
+      }
+    ])
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/*<h1>Use State with Object </h1>
+
+    <h2>First name is {values.fname} and Last name is {values.lname}</h2>
+    <button onClick={update}>Update</button>
+    */}
+
+
+      <h1>Use State with Arrays </h1>
+      <ol>
+      {
+        items.map((elements=>{
+          return(
+            <li key={elements.id}> {elements.object}</li>
+          )
+        }))
+      }
+      <button onClick={additems}>Add Items</button>
+      </ol>
     </div>
   );
 }
